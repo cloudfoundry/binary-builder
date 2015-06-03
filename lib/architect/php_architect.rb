6393be0cd13aa629ef094cb5de422232c74082b2
JT Archie <jtarchie@gmail.com>
2015-06-03 17:53:08 -0400

    use object to represent PHP extensions
    removed libmemcached in process
    
    Signed-off-by: Colin Jackson <colin.s.jackson90@gmail.com>

--- lib/architect/php_architect.rb
+++ lib/architect/php_architect.rb
@@ -9,105 +9,104 @@ module BinaryBuilder
         ZTS_VERSION:          "20100525",
         RABBITMQ_C_VERSION:   "0.5.2",
         HIREDIS_VERSION:      "0.11.0",
-        LUA_VERSION:          "5.2.4",
-        LIBMEMCACHED_VERSION: "1.0.18"
+        LUA_VERSION:          "5.2.4"
       },
       '5.5' => {
         ZTS_VERSION:          "20121212",
         RABBITMQ_C_VERSION:   "0.5.2",
         HIREDIS_VERSION:      "0.11.0",
-        LUA_VERSION:          "5.2.4",
-        LIBMEMCACHED_VERSION: "1.0.18"
+        LUA_VERSION:          "5.2.4"
       },
       '5.6' => {
         ZTS_VERSION:          "20131226",
         RABBITMQ_C_VERSION:   "0.5.2",
         HIREDIS_VERSION:      "0.11.0",
-        LUA_VERSION:          "5.2.4",
-        LIBMEMCACHED_VERSION: "1.0.18"
+        LUA_VERSION:          "5.2.4"
       }
     }
 
-    EXTERNAL_MODULES = {
-      '5.4' => {
-        amqp:            "1.4.0",
-        APC:             "3.1.9",
-        apcu:            "4.0.7",
-        igbinary:        "1.2.1",
-        imagick:         "3.1.2",
-        intl:            "3.0.0",
-        ioncube:         "4.7.5",
-        lua:             "1.1.0",
-        mailparse:       "2.1.6",
-        memcache:        "2.2.7",
-        memcached:       "2.2.0",
-        mongo:           "1.6.5",
-        msgpack:         "0.5.5",
-        phpiredis:       "trunk",
-        phalcon:         "1.3.4",
-        protocolbuffers: "0.2.6",
-        protobuf:        "trunk",
-        redis:           "2.2.5",
-        suhosin:         "0.9.37.1",
-        sundown:         "0.3.11",
-        twig:            "1.18.0",
-        xcache:          "3.2.0",
-        xdebug:          "2.3.1",
-        xhprof:          "trunk",
-        yaf:             "2.2.9",
-        zendopcache:     "7.0.4",
-        zookeeper:       "0.2.2",
-        libmemcached:    "1.0.18"
-      },
-      '5.5' => {
-        amqp:            "1.4.0",
-        igbinary:        "1.2.1",
-        imagick:         "3.1.2",
-        intl:            "3.0.0",
-        ioncube:         "4.7.5",
-        lua:             "1.1.0",
-        mailparse:       "2.1.6",
-        memcache:        "2.2.7",
-        memcached:       "2.2.0",
-        mongo:           "1.6.5",
-        msgpack:         "0.5.5",
-        phpiredis:       "trunk",
-        phalcon:         "1.3.4",
-        protocolbuffers: "0.2.6",
-        protobuf:        "trunk",
-        redis:           "2.2.5",
-        suhosin:         "0.9.37.1",
-        sundown:         "0.3.11",
-        twig:            "1.18.0",
-        xcache:          "3.2.0",
-        xdebug:          "2.3.1",
-        xhprof:          "trunk",
-        yaf:             "2.2.9"
-      },
-      '5.6' => {
-        amqp:            "1.4.0",
-        igbinary:        "1.2.1",
-        imagick:         "3.1.2",
-        intl:            "3.0.0",
-        ioncube:         "4.7.5",
-        lua:             "1.1.0",
-        mailparse:       "2.1.6",
-        memcache:        "2.2.7",
-        memcached:       "2.2.0",
-        mongo:           "1.6.5",
-        msgpack:         "0.5.5",
-        phpiredis:       "trunk",
-        phalcon:         "1.3.4",
-        protocolbuffers: "0.2.6",
-        protobuf:        "trunk",
-        redis:           "2.2.5",
-        suhosin:         "0.9.37.1",
-        sundown:         "0.3.11",
-        twig:            "1.18.0",
-        xcache:          "3.2.0",
-        xdebug:          "2.3.1",
-        yaf:             "2.3.3"
-      }
+    class PHPExtension < Struct.new(:name, :version, :package_php_extension_args)
+      def package_php_extension_args
+        super || [ name ]
+      end
+    end
+
+    AMQPExtension = PHPExtension.new('amqp', '1.4.0', ['amqp', "$APP_DIR/librmq-$RABBITMQ_C_VERSION/lib/librabbitmq.so.1"])
+    IntlExtension = PHPExtension.new('intl', '3.0.0', ['intl', "libicui18n.so.52", "libicuuc.so.52", "libicudata.so.52", "libicuio.so.52"])
+    MemcachedExtension = PHPExtension.new('memcached', '2.2.0', ['memcached', "libmemcached.so.10"])
+    PHPIRedisExtension = PHPExtension.new('phpiredis', 'trunk', ['phpiredis', "$APP_DIR/hiredis-$HIREDIS_VERSION/lib/libhiredis.so.0.10"])
+
+    EXTERNAL_EXTENSIONS = {
+      '5.4' => [
+        AMQPExtension, IntlExtension, MemcachedExtension, PHPIRedisExtension,
+        PHPExtension.new('APC', '3.1.9', ['apc']),
+        PHPExtension.new('apcu', '4.0.7'),
+        PHPExtension.new('igbinary', '1.2.1'),
+        PHPExtension.new('imagick', '3.1.2'),
+        PHPExtension.new('ioncube', '4.7.5'),
+        PHPExtension.new('lua', '1.1.0'),
+        PHPExtension.new('mailparse', '2.1.6'),
+        PHPExtension.new('memcache', '2.2.7'),
+        PHPExtension.new('mongo', '1.6.5'),
+        PHPExtension.new('msgpack', '0.5.5'),
+        PHPExtension.new('phalcon', '1.3.4'),
+        PHPExtension.new('protocolbuffers', '0.2.6'),
+        PHPExtension.new('protobuf', 'trunk'),
+        PHPExtension.new('redis', '2.2.5'),
+        PHPExtension.new('suhosin', '0.9.37.1'),
+        PHPExtension.new('sundown', '0.3.11'),
+        PHPExtension.new('twig', '1.18.0'),
+        PHPExtension.new('xcache', '3.2.0'),
+        PHPExtension.new('xdebug', '2.3.1'),
+        PHPExtension.new('xhprof', 'trunk'),
+        PHPExtension.new('yaf', '2.2.9'),
+        PHPExtension.new('zendopcache', '7.0.4', ['opcache']),
+        PHPExtension.new('zookeeper', '0.2.2')
+      ],
+      '5.5' => [
+        AMQPExtension, IntlExtension, MemcachedExtension, PHPIRedisExtension,
+        PHPExtension.new('APC', '3.1.9', ['apc']),
+        PHPExtension.new('igbinary', '1.2.1'),
+        PHPExtension.new('imagick', '3.1.2'),
+        PHPExtension.new('ioncube', '4.7.5'),
+        PHPExtension.new('lua', '1.1.0'),
+        PHPExtension.new('mailparse', '2.1.6'),
+        PHPExtension.new('memcache', '2.2.7'),
+        PHPExtension.new('mongo', '1.6.5'),
+        PHPExtension.new('msgpack', '0.5.5'),
+        PHPExtension.new('phalcon', '1.3.4'),
+        PHPExtension.new('protocolbuffers', '0.2.6'),
+        PHPExtension.new('protobuf', 'trunk'),
+        PHPExtension.new('redis', '2.2.5'),
+        PHPExtension.new('suhosin', '0.9.37.1'),
+        PHPExtension.new('sundown', '0.3.11'),
+        PHPExtension.new('twig', '1.18.0'),
+        PHPExtension.new('xcache', '3.2.0'),
+        PHPExtension.new('xdebug', '2.3.1'),
+        PHPExtension.new('xhprof', 'trunk'),
+        PHPExtension.new('yaf', '2.2.9')
+      ],
+      '5.6' => [
+        AMQPExtension, IntlExtension, MemcachedExtension, PHPIRedisExtension,
+        PHPExtension.new('igbinary', '1.2.1'),
+        PHPExtension.new('imagick', '3.1.2'),
+        PHPExtension.new('ioncube', '4.7.5'),
+        PHPExtension.new('lua', '1.1.0'),
+        PHPExtension.new('mailparse', '2.1.6'),
+        PHPExtension.new('memcache', '2.2.7'),
+        PHPExtension.new('mongo', '1.6.5'),
+        PHPExtension.new('msgpack', '0.5.5'),
+        PHPExtension.new('phalcon', '1.3.4'),
+        PHPExtension.new('protocolbuffers', '0.2.6'),
+        PHPExtension.new('protobuf', 'trunk'),
+        PHPExtension.new('redis', '2.2.5'),
+        PHPExtension.new('suhosin', '0.9.37.1'),
+        PHPExtension.new('sundown', '0.3.11'),
+        PHPExtension.new('twig', '1.18.0'),
+        PHPExtension.new('xcache', '3.2.0'),
+        PHPExtension.new('xdebug', '2.3.1'),
+        PHPExtension.new('yaf', '2.3.3')
+      ]
     }
 
     def blueprint
@@ -131,17 +130,8 @@ module BinaryBuilder
         @binary_version = options[:binary_version]
       end
 
-      def external_modules
-        EXTERNAL_MODULES[@minor_version]
-      end
-
-      def external_module_dependencies
-        {
-          amqp:      ["$APP_DIR/librmq-$RABBITMQ_C_VERSION/lib/librabbitmq.so.1"],
-          intl:      ["libicui18n.so.52", "libicuuc.so.52", "libicudata.so.52", "libicuio.so.52"],
-          memcached: ["libmemcached.so.10"],
-          phpiredis: ["$APP_DIR/hiredis-$HIREDIS_VERSION/lib/libhiredis.so.0.10"]
-        }
+      def external_extensions
+        EXTERNAL_EXTENSIONS[@minor_version]
       end
 
       def external_libraries
--- spec/unit/architects/php_architect_spec.rb
+++ spec/unit/architects/php_architect_spec.rb
@@ -3,31 +3,45 @@ require 'spec_helper'
 module BinaryBuilder
   describe PHPArchitect do
     subject(:architect) { PHPArchitect.new(binary_version: binary_version) }
-    let(:binary_version) { '5.6.9' }
 
-    describe 'blueprint' do
-      it 'adds the BINARY_VERSION value' do
-        expect(architect.blueprint).to include 'PHP_VERSION=5.6.9'
+    context 'when building any php interpreter and modules' do
+      let(:binary_version) { '5.6.9' }
+
+      describe 'blueprint' do
+        it 'adds the BINARY_VERSION value' do
+          expect(architect.blueprint).to include 'PHP_VERSION=5.6.9'
+        end
+
+        it 'adds the ZTS_VERSION value' do
+          expect(architect.blueprint).to include 'ZTS_VERSION=20131226'
+        end
+
+        it 'adds the RABBITMQ_C_VERSION value' do
+          expect(architect.blueprint).to include 'RABBITMQ_C_VERSION=0.5.2'
+        end
+
+        it 'adds the HIREDIS_VERSION value' do
+          expect(architect.blueprint).to include 'HIREDIS_VERSION=0.11.0'
+        end
+
+        it 'adds the LUA_VERSION value' do
+          expect(architect.blueprint).to include 'LUA_VERSION=5.2.4'
+        end
+
+        it 'adds the module version values' do
+          expect(architect.blueprint).to include %q{MODULES[amqp]="1.4.0"}
+        end
       end
+    end
 
-      it 'adds the ZTS_VERSION value' do
-        expect(architect.blueprint).to include 'ZTS_VERSION=20131226'
-      end
+    context 'when building php 5.4' do
+      let(:binary_version) { '5.4.41' }
 
-      it 'adds the RABBITMQ_C_VERSION value' do
-        expect(architect.blueprint).to include 'RABBITMQ_C_VERSION=0.5.2'
-      end
-
-      it 'adds the HIREDIS_VERSION value' do
-        expect(architect.blueprint).to include 'HIREDIS_VERSION=0.11.0'
-      end
-
-      it 'adds the LUA_VERSION value' do
-        expect(architect.blueprint).to include 'LUA_VERSION=5.2.4'
-      end
-
-      it 'adds the module version values' do
-        expect(architect.blueprint).to include %q{MODULES[amqp]="1.4.0"}
+      describe 'blueprint' do
+        it 'does not include the zendopcache as a package_php_extension because the *.so is opcache.so' do
+          expect(architect.blueprint).to_not include 'package_php_extension "zendopcache"'
+          expect(architect.blueprint).to include 'package_php_extension "opcache"'
+        end
       end
     end
   end
