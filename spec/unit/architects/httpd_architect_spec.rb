b0ef8064cef8565125ed0f8bcdaa7c2577351fb7
JT Archie <jtarchie@gmail.com>
2015-06-03 17:51:05 -0400

    move spec to correct location

--- spec/unit/architects/httpd_architect_spec.rb
+++ spec/unit/architects/httpd_architect_spec.rb
@@ -19,7 +19,7 @@ module BinaryBuilder
       end
 
       it 'uses the httpd_blueprint template' do
-        expect(File).to receive(:open).with(File.expand_path('../../../templates/httpd_blueprint', __FILE__))
+        expect(File).to receive(:open).with(File.expand_path('../../../../templates/httpd_blueprint', __FILE__))
         architect.blueprint
       end
 
