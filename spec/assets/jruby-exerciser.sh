5cd9047416544df0fb97990c7b913ba553d79a40
JT Archie <jtarchie@gmail.com>
2015-06-03 09:17:28 -0400

    jruby uses relative paths when executing
    Signed-off-by: Anthony Emengo <aemengo@pivotal.io>

--- spec/assets/jruby-exerciser.sh
+++ spec/assets/jruby-exerciser.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 set +e
 
+current_dir=`pwd`
+
 wget https://pivotal-buildpacks.s3.amazonaws.com/ruby/binaries/cflinuxfs2/openjdk1.8-latest.tar.gz
 mkdir openjdk
 tar xzf openjdk1.8-latest.tar.gz -C openjdk
-export PATH=`pwd`/openjdk/bin:$PATH
+export PATH=$current_dir/openjdk/bin:$PATH
 
 mkdir binary-exerciser
 cd binary-exerciser
 
-tar xzf /binary-builder/jruby-ruby-2.2.0-jruby-9.0.0.0.pre1-linux-x64.tgz
+tar xzf $current_dir/jruby-ruby-2.2.0-jruby-9.0.0.0.pre1-linux-x64.tgz
 ./bin/jruby -e 'puts "#{RUBY_PLATFORM} #{RUBY_VERSION}"'
