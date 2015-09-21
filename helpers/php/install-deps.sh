076b35f687766f17c21ef47d645444a510cf75aa
David Morhovich <dmorhovich@pivotal.io>
2015-09-21 11:32:27 -0400

    Removes deps that are already in cflinuxfs2.
    [#103324038]

--- helpers/php/install-deps.sh
+++ helpers/php/install-deps.sh
@@ -2,42 +2,29 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install \
-    autoconf \
     automake \
-    build-essential \
-    imagemagick \
     libaspell-dev \
-    libbz2-dev \
     libc-client2007e-dev \
     libcurl4-openssl-dev \
     libexpat1-dev \
     libgdbm-dev \
     libgmp-dev \
-    libicu-dev \
     libjpeg-dev \
     libldap2-dev \
-    libmagickcore-dev \
-    libmagickwand-dev \
     libmcrypt-dev \
     libmemcached-dev \
-    libmysqlclient-dev \
-    libpcre3-dev \
     libpng12-dev \
-    libpq-dev \
     libpspell-dev \
     libreadline-dev \
     libsasl2-dev \
     libsnmp-dev \
     libsqlite3-dev \
     libssl-dev \
-    libxml2-dev \
-    libxslt1-dev \
-    libyaml-dev \
     libzip-dev \
     libzookeeper-mt-dev \
     mercurial \
-    snmp-mibs-downloader \
-    unzip
+    snmp-mibs-downloader
+
 
 # Ubuntu 14.04 puts these headers in weird locations, need to add symlinks so PHP finds them
 sudo ln -fs /usr/include/x86_64-linux-gnu/gmp.h /usr/include/gmp.h
