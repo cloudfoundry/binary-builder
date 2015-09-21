e9cab61d130a36976f1c76ec80c58e4307202d49
David Morhovich <dmorhovich@pivotal.io>
2015-09-21 10:59:50 -0400

    Formats apt-get installed deps so that they're more readable.
    [#103324038]

--- helpers/php/install-deps.sh
+++ helpers/php/install-deps.sh
@@ -1,7 +1,43 @@
 # update repo and packages
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install build-essential autoconf automake libssl-dev libsnmp-dev snmp-mibs-downloader mercurial libbz2-dev libldap2-dev libpcre3-dev libxml2-dev libpq-dev libzip-dev libcurl4-openssl-dev libgdbm-dev libmysqlclient-dev libgmp-dev libjpeg-dev libpng12-dev libc-client2007e-dev libsasl2-dev libmcrypt-dev libaspell-dev libpspell-dev libexpat1-dev imagemagick libmagickwand-dev libmagickcore-dev unzip libmemcached-dev libicu-dev libsqlite3-dev libzookeeper-mt-dev libreadline-dev libxslt1-dev libyaml-dev
+sudo apt-get -y install \
+    autoconf \
+    automake \
+    build-essential \
+    imagemagick \
+    libaspell-dev \
+    libbz2-dev \
+    libc-client2007e-dev \
+    libcurl4-openssl-dev \
+    libexpat1-dev \
+    libgdbm-dev \
+    libgmp-dev \
+    libicu-dev \
+    libjpeg-dev \
+    libldap2-dev \
+    libmagickcore-dev \
+    libmagickwand-dev \
+    libmcrypt-dev \
+    libmemcached-dev \
+    libmysqlclient-dev \
+    libpcre3-dev \
+    libpng12-dev \
+    libpq-dev \
+    libpspell-dev \
+    libreadline-dev \
+    libsasl2-dev \
+    libsnmp-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libxml2-dev \
+    libxslt1-dev \
+    libyaml-dev \
+    libzip-dev \
+    libzookeeper-mt-dev \
+    mercurial \
+    snmp-mibs-downloader \
+    unzip
 
 # Ubuntu 14.04 puts these headers in weird locations, need to add symlinks so PHP finds them
 sudo ln -fs /usr/include/x86_64-linux-gnu/gmp.h /usr/include/gmp.h
