f074a7ccd0f86c46bd644018e4bcb18a02215dbc
Amin Jams <aminjam.software@gmail.com>
2015-11-10 09:39:06 -0500

    Remove automake and mercurial from PHP meal
    Remove automake and mercurial from PHP meal
    [#107207422]
    (https://www.pivotaltracker.com/story/show/107207422)
    
    Signed-off-by: Brandon Shroyer <bshroyer@pivotal.io>

--- recipe/php_meal.rb
+++ recipe/php_meal.rb
@@ -332,7 +332,6 @@ class PhpMeal
       sudo apt-get update
       sudo apt-get -y upgrade
       sudo apt-get -y install \
-        automake \
         libaspell-dev \
         libc-client2007e-dev \
         libcurl4-openssl-dev \
@@ -352,7 +351,6 @@ class PhpMeal
         libssl-dev \
         libzip-dev \
         libzookeeper-mt-dev \
-        mercurial \
         snmp-mibs-downloader
       sudo ln -fs /usr/include/x86_64-linux-gnu/gmp.h /usr/include/gmp.h
       sudo ln -fs /usr/lib/x86_64-linux-gnu/libldap.so /usr/lib/libldap.so
