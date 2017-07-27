eac035b8dc4c8b81488a977311739723a5c6d62c
Anna Thornton <athornton@pivotal.io>
2017-07-27 12:05:44 -0400

    Bump openjdk to 1.8.0_141 and update opnjdk shasum
    [#149546525]
    
    Signed-off-by: Forest Eckhardt <forest.eckhardt@gmail.com>

--- recipe/jruby_meal.rb
+++ recipe/jruby_meal.rb
@@ -21,8 +21,8 @@ class JRubyMeal
     java_jdk_dir = '/opt/java'
     java_jdk_tar_file = File.join(java_jdk_dir, 'openjdk-8-jdk.tar.gz')
     java_jdk_bin_dir = File.join(java_jdk_dir, 'bin')
-    java_jdk_sha256 = '1315567082b55b3e1a62156d36c6f8adad152c32ab4a9eed7e72c1b24c381f9e'
-    java_buildpack_java_sdk = "https://java-buildpack.cloudfoundry.org/openjdk-jdk/trusty/x86_64/openjdk-1.8.0_131.tar.gz"
+    java_jdk_sha256 = '14016fe0b8005c60013f2c418456ae1e46a8c46569db2900b1eaca589230afcd'
+    java_buildpack_java_sdk = "https://java-buildpack.cloudfoundry.org/openjdk-jdk/trusty/x86_64/openjdk-1.8.0_141.tar.gz"
 
     FileUtils.mkdir_p(java_jdk_dir)
     raise "Downloading openjdk-8-jdk failed." unless system("wget #{java_buildpack_java_sdk} -O #{java_jdk_tar_file}")
