63b2e38db75ac1d9d8b4cef644faef7a5fed4589
Sam Coward <scoward@pivotal.io>
2017-09-08 13:25:19 -0400

    Fix tarball name and binary-exerciser test for dep
    [#149822182]

--- spec/integration/dep_spec.rb
+++ spec/integration/dep_spec.rb
@@ -6,7 +6,7 @@ describe 'building a binary', :integration do
   context 'when dep is specified' do
     before(:all) do
       run_binary_builder('dep', 'v0.3.0', '--sha256=7d816ffb14f57c4b01352676998a8cda9e4fb24eaec92bd79526e1045c5a0c83')
-      @binary_tarball_location = File.join(Dir.pwd, 'dep-v0.3.0-linux-amd64.tar.gz')
+      @binary_tarball_location = File.join(Dir.pwd, 'dep-v0.3.0-linux-x64.tgz')
     end
 
     after(:all) do
@@ -16,11 +16,11 @@ describe 'building a binary', :integration do
     it 'builds the specified binary, tars it, and places it in your current working directory' do
       expect(File).to exist(@binary_tarball_location)
 
-      dep_version_cmd = './spec/assets/binary-exerciser.sh dep-v0.3.0-linux-amd64.tar.gz ./bin/dep version'
+      dep_version_cmd = './spec/assets/binary-exerciser.sh dep-v0.3.0-linux-x64.tgz ./bin/dep ensure -examples'
       output, status = run(dep_version_cmd)
 
       expect(status).to be_success
-      expect(output).to include('v0.3.0')
+      expect(output).to include('dep ensure')
     end
 
     it 'includes the license in the tar file.' do
