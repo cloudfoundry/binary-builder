ee48a6f1f456f40587ed2936d81c7ff7531300b3
Dave Goddard <dave@goddard.id.au>
2017-12-08 13:41:07 -0500

    Include tcl/tk in built pythons [#153231455]

--- recipe/python.rb
+++ recipe/python.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+require 'fileutils'
 require 'mini_portile'
 require_relative 'base'
 
@@ -8,13 +9,22 @@ class PythonRecipe < BaseRecipe
       '--enable-shared',
       '--with-ensurepip=no',
       '--with-dbmliborder=bdb:gdbm',
+      '--with-tcltk-includes="-I/usr/include/tcl8.6"',
+      '--with-tcltk-libs="-L/usr/lib/x86_64-linux-gnu -ltcl8.6 -L/usr/lib/x86_64-linux-gnu -ltk8.6"',
       "--prefix=#{prefix_path}",
       '--enable-unicode=ucs4'
     ]
   end
 
   def cook
-    install_apt('libdb-dev libgdbm-dev')
+    install_apt('libdb-dev libgdbm-dev tk8.6-dev')
+
+    run('apt-get -y --force-yes -d install --reinstall libtcl8.6 libtk8.6 libxss1') or raise 'Failed to download libtcl8.6 libtk8.6 libxss1'
+    FileUtils.mkdir_p prefix_path
+    Dir.glob('/var/cache/apt/archives/lib{tcl8.6,tk8.6,xss1}_*.deb').each do |path|
+      STDOUT.puts("dpkg -x #{path} #{prefix_path}")
+      run("dpkg -x #{path} #{prefix_path}") or raise "Could not extract #{path}"
+    end
 
     super
   end
@@ -41,14 +51,21 @@ class PythonRecipe < BaseRecipe
 
   def install_apt(packages)
     STDOUT.print "Running 'install dependencies' for #{@name} #{@version}... "
-    STDOUT.flush
-    apt_output = `sudo apt-get update && sudo apt-get -y install #{packages}`
-    if $?.success?
+    if run("sudo apt-get update && sudo apt-get -y install #{packages}")
       STDOUT.puts "OK"
     else
-      STDOUT.puts "ERROR, output was:"
-      STDOUT.puts apt_output
       raise "Failed to complete install dependencies task"
+    end
+  end
+
+  def run(command)
+    output = `#{command}`
+    if $?.success?
+      return true
+    else
+      STDOUT.puts "ERROR, output was:"
+      STDOUT.puts output
+      return false
     end
   end
 end
