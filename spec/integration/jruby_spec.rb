b59a127ada48be9ea30d153b5622f88ece570354
Leah Hanson <lhanson@pivotal.io>
2018-07-03 15:19:40 -0400

    Trying again to make specs match jruby recipe

--- spec/integration/jruby_spec.rb
+++ spec/integration/jruby_spec.rb
@@ -5,7 +5,7 @@ require 'fileutils'
 describe 'building a binary', :integration do
   context 'when jruby is specified' do
     before(:all) do
-      output = run_binary_builder('jruby', '9.1.17.0', '--sha256=b66d7c14f85075afdabb5ebf5950804c5a5d5c1d05ab833f580f04ee709b5773')
+      output = run_binary_builder('jruby', '9.1.17.0_ruby-2.3', '--sha256=b66d7c14f85075afdabb5ebf5950804c5a5d5c1d05ab833f580f04ee709b5773')
       @binary_tarball_location = File.join(Dir.pwd, 'jruby-9.1.17.0_ruby-2.3-linux-x64.tgz')
     end
 
