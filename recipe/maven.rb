48582c2e44c6c31b2da7e2e064835619f1ab6bc9
Don Nguyen <dnguyen@pivotal.io>
2019-04-25 12:31:24 -0400

    Change maven version to 3.6.1
    - Use SHA512 from the apache distribution page instead of manually calculating SHA256
    
    [#165329634]
    
    Co-authored-by: Tilly Taylor <mataylor@pivotal.io>

--- recipe/maven.rb
+++ recipe/maven.rb
@@ -10,29 +10,29 @@ class MavenRecipe < BaseRecipe
     download unless downloaded?
     extract
 
-    #install maven 3.3.9 to $HOME/apache-maven-3.3.9
-    mvn339_sha256 = '6e3e9c949ab4695a204f74038717aa7b2689b1be94875899ac1b3fe42800ff82'
+    #install maven 3.6.1 to $HOME/apache-maven-3.6.1
+    mvn361_sha512 = 'b4880fb7a3d81edd190a029440cdf17f308621af68475a4fe976296e71ff4a4b546dd6d8a58aaafba334d309cc11e638c52808a4b0e818fc0fd544226d952544'
 
     Dir.chdir("#{ENV['HOME']}") do
-      maven_download = 'https://www.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz'
-      maven_tar = "apache-maven-3.3.9-bin.tar.gz"
+      maven_download = 'https://www.apache.org/dist/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz'
+      maven_tar = "apache-maven-3.6.1-bin.tar.gz"
       system("curl -L #{maven_download} -o #{maven_tar}")
 
-      downloaded_sha = Digest::SHA256.file(maven_tar).hexdigest
+      downloaded_sha = Digest::SHA512.file(maven_tar).hexdigest
 
-      if mvn339_sha256 != downloaded_sha
-        raise "sha256 verification failed: expected #{go_sha256}, got #{downloaded_sha}"
+      if mvn361_sha512 != downloaded_sha
+        raise "sha512 verification failed: expected #{mvn361_sha512}, got #{downloaded_sha}"
       end
 
       system("tar xf #{maven_tar}")
     end
 
     old_path = ENV['PATH']
-    ENV['PATH'] = "#{ENV['HOME']}/apache-maven-3.3.9/bin:#{old_path}"
+    ENV['PATH'] = "#{ENV['HOME']}/apache-maven-3.6.1/bin:#{old_path}"
 
     install
     ENV['PATH'] = old_path
-    FileUtils.rm_rf(File.join(ENV['HOME'], 'apache-maven-3.3.9'))
+    FileUtils.rm_rf(File.join(ENV['HOME'], 'apache-maven-3.6.1'))
   end
 
   def install
