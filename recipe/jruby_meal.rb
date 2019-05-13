c76f3d73dd1fef25d0820120833bb1e7dea25855
Chhavi Kankaria <ckankaria@pivotal.io>
2019-05-13 11:35:42 -0400

    Update openjdk version to latest 1.8 for jruby [#165405673] Co-authored-by: Tilly Taylor <mataylor@pivotal.io>

--- recipe/jruby_meal.rb
+++ recipe/jruby_meal.rb
@@ -21,8 +21,8 @@ class JRubyMeal
     java_jdk_dir = '/opt/java'
     java_jdk_tar_file = File.join(java_jdk_dir, 'openjdk-8-jdk.tar.gz')
     java_jdk_bin_dir = File.join(java_jdk_dir, 'bin')
-    java_jdk_sha256 = '6254bacb4d9e6cae1e8fdd44f06bbb2c6f26081f937e3786e6c53aea57f94a4d'
-    java_buildpack_java_sdk = "https://java-buildpack.cloudfoundry.org/openjdk-jdk/trusty/x86_64/openjdk-1.8.0_192.tar.gz"
+    java_jdk_sha256 = '0749e51942641f369a9411ba27207fbf74b8836f1fb19a3d85cee55e13387a9c'
+    java_buildpack_java_sdk = "https://java-buildpack.cloudfoundry.org/openjdk-jdk/trusty/x86_64/openjdk-jdk-1.8.0_212-trusty.tar.gz"
 
     FileUtils.mkdir_p(java_jdk_dir)
     raise "Downloading openjdk-8-jdk failed." unless system("wget #{java_buildpack_java_sdk} -O #{java_jdk_tar_file}")
