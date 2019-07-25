4b42a99589996d73045b82120c1e8333b3d41224
Daniel Thornton <dthornton@pivotal.io>
2019-07-25 11:59:12 -0400

    Update the sha of java sdk
    [#167336019]
    
    Co-authored-by: Don Nguyen <dnguyen@pivotal.io>

--- recipe/jruby_meal.rb
+++ recipe/jruby_meal.rb
@@ -21,7 +21,7 @@ class JRubyMeal
     java_jdk_dir = '/opt/java'
     java_jdk_tar_file = File.join(java_jdk_dir, 'openjdk-8-jdk.tar.gz')
     java_jdk_bin_dir = File.join(java_jdk_dir, 'bin')
-    java_jdk_sha256 = '0749e51942641f369a9411ba27207fbf74b8836f1fb19a3d85cee55e13387a9c'
+    java_jdk_sha256 = '1210b14353f9a911aad84182e36be229a30991c5ca6b8b1a932aad426e106276'
     java_buildpack_java_sdk = "https://java-buildpack.cloudfoundry.org/openjdk-jdk/trusty/x86_64/openjdk-jdk-1.8.0_222-trusty.tar.gz"
 
     FileUtils.mkdir_p(java_jdk_dir)
