af9ae91e087b0aa270ec1dcb55193449580f8ed8
Zander Mackie <amackie@pivotal.io>
2019-12-05 15:27:06 -0500

    Fix maven 3.6.3 sha512
    [#169962350]

--- recipe/maven.rb
+++ recipe/maven.rb
@@ -10,12 +10,12 @@ class MavenRecipe < BaseRecipe
     download unless downloaded?
     extract
 
-    #install maven 3.6.1 to $HOME/apache-maven-3.6.1
-    sha512 = '14eef64ad13c1f689f2ab0d2b2b66c9273bf336e557d81d5c22ddb001c47cf51f03bb1465d6059ce9fdc2e43180ceb0638ce914af1f53af9c2398f5d429f114c'
+    #install maven 3.6.3 to $HOME/apache-maven-3.6.3
+    sha512 = 'c35a1803a6e70a126e80b2b3ae33eed961f83ed74d18fcd16909b2d44d7dada3203f1ffe726c17ef8dcca2dcaa9fca676987befeadc9b9f759967a8cb77181c0'
 
     Dir.chdir("#{ENV['HOME']}") do
-      maven_download = 'https://archive.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.3-bin.tar.gz'
-      maven_tar = "apache-maven-3.6.3-bin.tar.gz"
+      maven_download = "https://archive.apache.org/dist/maven/maven-3/#{version}/binaries/apache-maven-#{version}-bin.tar.gz"
+      maven_tar = "apache-maven-#{version}-bin.tar.gz"
       system("curl -L #{maven_download} -o #{maven_tar}")
 
       downloaded_sha = Digest::SHA512.file(maven_tar).hexdigest
