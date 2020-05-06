68286db456c81fb3020a46c18c7d9ca8e027128f
Mark DeLillo <mdelillo@pivotal.io>
2020-05-06 15:29:16 -0400

    Fix ruby version in jruby specs

--- spec/integration/jruby_spec.rb
+++ spec/integration/jruby_spec.rb
@@ -5,8 +5,8 @@ require 'fileutils'
 describe 'building a binary', :integration do
   context 'when jruby is specified' do
     before(:all) do
-      output = run_binary_builder('jruby', '9.2.8.0_ruby-2.5', '--sha256=287ae0e946c2d969613465c738cc3b09098f9f25805893ab707dce19a7b98c43')
-      @binary_tarball_location = File.join(Dir.pwd, 'jruby-9.2.8.0_ruby-2.5-linux-x64.tgz')
+      output = run_binary_builder('jruby', '9.2.8.0-ruby-2.5', '--sha256=287ae0e946c2d969613465c738cc3b09098f9f25805893ab707dce19a7b98c43')
+      @binary_tarball_location = File.join(Dir.pwd, 'jruby-9.2.8.0-ruby-2.5-linux-x64.tgz')
     end
 
     after(:all) do
