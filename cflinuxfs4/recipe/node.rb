52d00050f3edea4af2982d44c05da3fd03abacbb
Brayan Henao <bhenao@vmware.com>
2022-08-24 14:45:48 -0400

    Remove Python reference when configuring Nodejs

--- cflinuxfs4/recipe/node.rb
+++ cflinuxfs4/recipe/node.rb
@@ -37,10 +37,6 @@ class NodeRecipe < BaseRecipe
   end
 
   def configure
-    if Gem::Version.new(version) >= Gem::Version.new('16.0.0')
-      execute('configure', %w[python3 configure] + computed_options)
-    else
-      execute('configure', %w[python configure] + computed_options)
-    end
+    execute('configure', %w(./configure) + computed_options)
   end
 end
