8226d45d93841434edd2812401703a4d88691eed
Forest Eckhardt <feckhardt@pivotal.io>
2024-10-03 22:07:45 +0000

    Updates the go installer

--- cflinuxfs4/lib/install_go.rb
+++ cflinuxfs4/lib/install_go.rb
@@ -1,13 +1,25 @@
 # frozen_string_literal: true
 require_relative 'utils'
+require 'net/http'
+require 'json'
 
 def install_go_compiler
-  go_compiler_info = YAML.load_file(File.join(__dir__, '..', 'go-version.yml'))['go'].first
-  go_version = go_compiler_info['version']
-  go_sha256 = go_compiler_info['sha256']
+  url = URI('https://go.dev/dl/?mode=json')
+  res = Net::HTTP.get(url)
+  latest_go = JSON.parse(res).first
+
+  go_version = latest_go['version'].delete_prefix('go')
+  go_sha256 = ""
+
+  latest_go['files'].each do |file|
+    if file['filename'] == "go#{go_version}.linux-amd64.tar.gz"
+      go_sha256 = file['sha256']
+      break
+    end
+  end
 
   Dir.chdir('/usr/local') do
-    go_download = "https://dl.google.com/go/go#{go_version}.linux-amd64.tar.gz"
+    go_download = "https://go.dev/dl/go#{go_version}.linux-amd64.tar.gz"
     go_tar = 'go.tar.gz'
 
     HTTPHelper.download(go_download, go_tar, "sha256", go_sha256)
